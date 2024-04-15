272990d9dc259df363c1a1f6f85632bd77a33446
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2024-04-15 23:42:58 +0300

    feat: implement getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -243,8 +243,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return function f() {
+    start += 1;
+    return start - 1;
+  };
 }
 
 module.exports = {
